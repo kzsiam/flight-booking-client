52f25933bde6abe95c9566374b234044fb3b7073
Kazee Siam <kzsiam@outlook.com>
2025-08-11 11:10:58 +0600

    client setup

---
+++ src/Layout/MainLayout/MainLayout.jsx
@@ -0,0 +1,21 @@
+import { Link, Outlet } from 'react-router-dom';
+import Footer from '../../SharedPages/Footer/Footer';
+import Navbar from '../../SharedPages/Navbar/Navbar';
+
+const MainLayout = () => {
+    return (
+        <div className='w-full'>
+            <div>
+                <Navbar></Navbar>
+            </div>
+            <div className='container mx-auto'>
+                <Outlet></Outlet>
+            </div>
+            <div>
+                <Footer></Footer>
+            </div>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/Routes/Router/Router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../../Layout/MainLayout/MainLayout";
+import Home from "../../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+   {
+    path: '/',
+    element:<MainLayout></MainLayout>,
+    children:[
+        {
+            path: '/',
+            element:<Home></Home>
+        },
+        {
+            path: '/signin',
+            element:<h1>this is sign in</h1>
+        }
+    ]
+   } 
+])
